f14799ea959ac4aaa35e064dc0005ecfe2e55697
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-10-08 00:16:42 +0530

    Enhance HeroSection with video background and animation
    Updated the HeroSection component to include a video background, animated headline using react-type-animation, and improved layout and styling. Integrated the new HeroSection into the Home screen for a more engaging landing experience.

--- techknowlk_web/src/screens/Home.jsx
+++ techknowlk_web/src/screens/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { NavBar } from '../components/NavBar'
+import HeroSection from '../components/Home/HeroSection'
 
 export const Home = () => {
   return (
     <div className='relative w-full'>
        <NavBar/>
+       <HeroSection/>
     </div>
   )
 }
