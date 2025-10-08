ff0222fd0b0b4821fc110cf0df8a2b6a082a473e
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-10-08 10:06:24 +0530

    Create Products.jsx

---
+++ techknowlk_web/src/screens/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavBar } from '../components/NavBar'
+
+export default function Products() {
+  return (
+    <div>
+        <NavBar/>
+        <h1>Products</h1>
+    </div>
+  )
+}
