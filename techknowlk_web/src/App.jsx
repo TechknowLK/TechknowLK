889c3dcaa290a7298b553c250c938fdd9682fe46
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-10-08 10:06:02 +0530

    Update App.jsx

--- techknowlk_web/src/App.jsx
+++ techknowlk_web/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './screens/Home';
 import { AboutUs } from './screens/AboutUs';
+import Services from './screens/Services';
+import Products from './screens/Products';
+import Blogs from './screens/Blogs';
+import Contact from './screens/Contact';
 
 export const App = () => {
   return (
@@ -9,6 +13,10 @@ export const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<AboutUs />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/blogs' element={<Blogs />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </Router>
   )
