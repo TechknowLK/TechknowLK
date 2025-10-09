aa7961de84a280314eb2f0ad6320ce26b845b113
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-10-09 13:54:16 +0530

    Update OurStorySection.jsx

--- techknowlk_web/src/components/AboutUs/OurStorySection.jsx
+++ techknowlk_web/src/components/AboutUs/OurStorySection.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function OurStorySection() {
     return (
-        <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16 my-16 '>
+        <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  mt-16  '>
             <div className='w-full'>
-                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight drop-shadow-sm">
+                <h1 className="text-3xl px-4 md:text-4xl lg:text-5xl font-bold leading-tight drop-shadow-sm">
                     <span className="text-[#626262]">Our </span>
                     <span className="text-[#33A1E0]">Story</span>
                 </h1>
                 <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div className="p-4 ">
-                        <div className='w-full h-full text-xl text-[#626262]'>
+                        <div className='w-full h-full text-base lg:text-xl text-[#626262]'>
 
                             Founded in 2020, TechknowLK emerged from a vision to bridge the gap between traditional business operations and cutting-edge technology. Our journey began with a small team of passionate engineers who believed that smart technology could transform businesses of all sizes.<br/><br/>
 
