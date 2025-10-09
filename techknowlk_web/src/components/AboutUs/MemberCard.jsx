7b817769558c74568d045765e57dcabff87b00c0
Supun Sulakshana <supunsulakshana2001@gmail.com>
2025-10-09 13:54:12 +0530

    Create MemberCard.jsx

---
+++ techknowlk_web/src/components/AboutUs/MemberCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+
+
+export default function MemberCard({ 
+    name, 
+    role, 
+    experience, 
+    description, 
+    image, 
+    bgColor = "#33A1E0" 
+}) {
+    return (
+        <div className="bg-card rounded-lg p-6 shadow-[10px_10px_25px_rgba(0,0,0,0.13)] transition-all duration-500 hover:scale-105 px-10">
+            <div className="flex flex-col items-center mb-4">
+                <div
+                    className="relative w-30 h-32 shadow shadow-gray-700 mb-2 rounded-2xl overflow-visible flex justify-center items-end mt-10"
+                    style={{ backgroundColor: bgColor }}
+                >
+                    <img
+                        src={image}
+                        alt={name}
+                        className="absolute bottom-0 h-[120%]  object-cover"
+                    />
+                </div>
+
+                <p className="text-muted-foreground text-sm lg:text-xl font-bold leading-relaxed text-[#626262]">
+                    {name}
+                </p>
+                <p className="text-muted-foreground text-sm lg:text-lg font-semibold leading-relaxed" style={{ color: bgColor }}>
+                    {role}
+                </p>
+                <p className="font-normal mb-6 text-sm text-foreground">{experience}</p>
+                <p className="font-normal text-[#626262] text-foreground">{description}</p>
+            </div>
+
+            <div className="flex items-center gap-3">
+                {/* Optional area for social links or icons */}
+            </div>
+        </div>
+    );
+}
